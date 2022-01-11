de63938ecd4e706daae7c0f568fa0bf80cd8fda9
kosaj <kosaj@users.noreply.github.com>
2022-01-12 00:08:21 +0100

    ....

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,42 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  ElementRef,
+  HostBinding,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { jsPDF } from 'jspdf';
+
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+var htmlToPdfmake = require('html-to-pdfmake');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   private readonly docInstance = new jsPDF();
   title = 'cvToPdf';
 
+  @ViewChild('kebab', { static: true }) kebab!: ElementRef;
+
   constructor() {
-    this.docInstance.save('a4.pdf');
+    (window as any).pdfMake.vfs = pdfFonts.pdfMake.vfs;
+  }
+  ngOnInit(): void {}
+
+  test() {
+    var html = htmlToPdfmake((this.kebab.nativeElement as Element).innerHTML);
+    console.log(html);
+    const documentDefinition = { content: html };
+    pdfMake.createPdf(documentDefinition).open();
   }
 }
+
+//https://github.com/Aymkdn/html-to-pdfmake
+
+//pdfmake types
+//https://www.npmjs.com/package/@types/pdfmake
