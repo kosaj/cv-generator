03807664d28d05a2c1ba97d35dcf4d2c3aa8d4fb
kosaj <kosaj@users.noreply.github.com>
2022-01-11 22:01:27 +0100

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { jsPDF } from 'jspdf';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  private readonly docInstance = new jsPDF();
   title = 'cvToPdf';
+
+  constructor() {
+    this.docInstance.save('a4.pdf');
+  }
 }
